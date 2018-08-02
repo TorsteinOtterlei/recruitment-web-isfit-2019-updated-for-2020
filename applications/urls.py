cb3e3c81cf885b399ce87fa473f29775a8957310
Peder Breivik Grundvold <pederbg@stud.ntnu.no>
2018-08-02 14:47:29 +0200

    Add url to applications

--- applications/urls.py
+++ applications/urls.py
@@ -5,9 +5,8 @@ app_name = 'applications'
 
 urlpatterns = [
     path('apply/', views.apply, name='apply'),
-    path('applications/', views.applications, name='applications'),
-    path('view_applications/', views.view_applications, name='view_applications'),
     path('applicant_text/<int:pk>', views.ApplicationDetail.as_view(), name='applicant_text'),
-    path('all_applications/', views.all_applications, name='all_applications'),
     path('set_dates/', views.set_dates, name='set_dates'),
+    path('manage_applications/', views.manage_applications, name='manage_applications'),
+    path('<int:user_id>', views.manage_profile, name='manage_profile'),
 ]
