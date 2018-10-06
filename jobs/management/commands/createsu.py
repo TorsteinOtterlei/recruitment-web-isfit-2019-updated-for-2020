fa50b143b99de988f5c3a23adc6c891674fe6c4f
Camilla <cammiida@gmail.com>
2018-10-06 16:45:36 +0200

    Changed createsu command

--- jobs/management/commands/createsu.py
+++ jobs/management/commands/createsu.py
@@ -6,6 +6,6 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         email = "admin@admin.com"
         pw = "admin"
-        if not User.objects.filter(email="").exists():
+        if not User.objects.filter(email=email).exists():
             User.objects.create_superuser(email=email, password=pw)
             print("Superuser created: email: {}, password: {}".format(email, pw))
