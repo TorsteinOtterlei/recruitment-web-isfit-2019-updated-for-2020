aaa2008595e106e39cce850e4b514cf57f64ffc3
Camilla <cammiida@gmail.com>
2018-10-06 18:16:01 +0200

    Change admin user credentials for deployment
    Import os

--- jobs/management/commands/createsu.py
+++ jobs/management/commands/createsu.py
@@ -1,11 +1,16 @@
 from django.core.management.base import BaseCommand
 from accounts.models import User
+import os
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        email = "admin@admin.com"
-        pw = "admin"
+        if os.environ['RDS_DB_NAME']:
+            email = os.environ['DJANGO_ADMIN_EMAIL']
+            pw = os.environ['DJANGO_ADMIN_PASSWORD']
+        else:
+            email = "admin@admin.com"
+            pw = "admin"
         if not User.objects.filter(email=email).exists():
             User.objects.create_superuser(email=email, password=pw)
             print("Superuser created: email: {}, password: {}".format(email, pw))
